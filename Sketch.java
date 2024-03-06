374c4374cf1ca8bf800bf0045f2fdf27b2fbaf04
Link1400 <ties_derby0d@icloud.com>
2024-03-05 20:39:52 -0500

    final commit

--- Sketch.java
+++ Sketch.java
@@ -9,6 +9,7 @@ public class Sketch extends PApplet {
   public void settings() {
 	// put your size call here
     size(400, 400);
+  
   }
 
   /** 
@@ -16,21 +17,47 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 120, 255);
+    noLoop();
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
+	
+  public void draw() {
+    
+    float random1 = random(0, 400);
+    float random2 = random(0, 200);
+    float random3 = random(0, 400);
+    float random4 = random(0, 400);
+	// sample code, delete this stuff
+    if (random1 < 200) {
+      background(0,120,255);
+    }
+    if (random1 > 200) {
+      background(255, 165, 0);
+    }
+    fill(255, 100, 0);
+    ellipse(random1, 20, 400, 400);
+    fill(255, 140, 0);
+    ellipse(random1, 20, 350, 350);
+    fill(255, 180, 0);
+    ellipse(random1, 20, 300, 300);
+    fill(255, 200, 0);
+    ellipse(random1, 20, 250, 250);
+    fill(255, 220, 0);
+    ellipse(random1, 20, 200, 200);
+    fill(255, 255, 0);
+    ellipse(random1, 20, 150, 150);
+    fill(120);
+    rect(random2 - 100, 250, 150, 400);
+    rect(random2 + 100, 250, 150, 400);
+    rect(random2, 200, 150, 400);
+    
+
+    
+    
+  }
 }
